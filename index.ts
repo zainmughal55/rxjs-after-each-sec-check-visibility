5a4803c205bb161da462303d13482bd114eee1a5
zainmughal55 <zainmughalkpk@gmail.com>
2021-01-21 14:12:09 +0500

    1 file modified

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { map, filter, delay, concatMap, switchMap, last } from "rxjs/operators";
 
 setTimeout(() => {
   var newItem = document.createElement("LI");
+  newItem.className = "test1";
   var textnode = document.createTextNode("Water");
   newItem.appendChild(textnode);
 
@@ -13,10 +14,13 @@ const source = of("a").pipe(
   switchMap(() => {
     return from(new Array(1, 2, 3, 4, 5, 6)).pipe(
       concatMap((i, index) => {
-        return of(i).pipe(delay(1000));
+        if (document.querySelector(".test1")) {
+          return of(true);
+        } else {
+          return of(false).pipe(delay(1000));
+        }
       }),
-      last(),
-      switchMap(() => of(10))
+      last()
     );
   })
 );
